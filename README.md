aef174194a2e78513c59b712216d5460d730ea40
Wir2494 <wirmatabadal@gmail.com>
2018-10-17 02:11:57 -0300

    Backend of the app is ready

---
+++ README.md
@@ -0,0 +1,2 @@
+#Shoppinglist
+##A place where you can write down your products you want to buy
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://Wiraash:password94@ds247170.mlab.com:47170/shoppinglist'
+}
